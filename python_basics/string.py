c6a3828a794f22082f63105b767747a8ff4d4e45
ShahriarHA <shahriar08neub@gmail.com>
2024-11-05 09:46:21 +0600

    string

---
+++ python_basics/string.py
@@ -0,0 +1,17 @@
+# strings are arrays in python
+a = "hello"
+print(a[1])
+
+# looping through a string
+for x in a:
+    print(x)
+
+print(len(a))
+
+# check string
+txt = "the best things in life are free!"
+if "free" in txt :
+    print("yes!")
+else:
+    print("No!")
+
