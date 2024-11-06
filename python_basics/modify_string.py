a00f0dd6fea558b4ede6fb1b6f38af9c27a453ad
ShahriarHA <shahriar08neub@gmail.com>
2024-11-06 14:59:05 +0600

    modify string

---
+++ python_basics/modify_string.py
@@ -0,0 +1,18 @@
+# upper case method
+str = "Hellow. World!"
+print(str.upper())
+print(str)
+# lower case method
+print(str.lower())
+# reomve whitespace
+str1 = " sha "
+print(str1)
+print(str1.strip())
+# split method
+print(str.split("."))
+str_2 = str.split(".")
+print(str_2)
+print(str_2[1].strip())
+
+
+
