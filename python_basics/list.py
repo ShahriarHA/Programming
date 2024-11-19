1dd9a5f265d700b0de71977435c61d6709ac0b59
ShahriarHA <shahriar08neub@gmail.com>
2024-11-19 19:19:01 +0600

    loopcomprehension

--- python_basics/list.py
+++ python_basics/list.py
@@ -26,4 +26,44 @@ print(list1)
 
 # note: The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
 
+# remove specified items from list
+re_list = ["apple", "banana", "cherry","kiwi"]
+re_list.remove("kiwi")
+print(re_list)
+# If there are more than one item with the specified value, the remove() method removes the first occurrence:
+# remove specified items using pop()
+
+re_list.pop(0)
+print(re_list)
+# If you do not specify the index, the pop() method removes the last item.
+
+# The clear() method empties the list.
+re_list.clear()
+print(re_list)
+
+# loop list-----
+l_list = ["cherry","banana","kiwi"]
+for x in l_list:
+    print(x)
+# loop through the index numbers
+for i in range(len(l_list)):
+    print(l_list[i])
+
+print("looping through list comprehension!")
+[print(x) for x in l_list]
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+new_f_list = []
+for x in fruits:
+    if "a" in x:
+        new_f_list.append(x)
+
+print(new_f_list)
+
+# using loop comprehension
+new_f_list.clear()
+new_f_list = [x for x in fruits if "a" in x]
+print(new_f_list)
+
+
 
