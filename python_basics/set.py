dce149f00f318b857f808d0d200e57c9f9263cde
ShahriarHA <shahriar08neub@gmail.com>
2024-11-28 20:33:33 +0600

    set added

---
+++ python_basics/set.py
@@ -0,0 +1,11 @@
+# A set is a collection which is unordered, unchangeable*, and unindexed.
+a_set = {"apple","banana","cherry"}
+print(a_set)
+print(len(a_set))
+print(type(a_set))
+
+# The set() Constructor
+set1 = set(("apple","banana","cherry",10,True))
+print(set1)
+
+
