5cce0029e887e539936b68325f62a243b7cba7aa
ShahriarHA <shahriar08neub@gmail.com>
2025-01-24 15:44:58 +0600

    hr-collectionsCounter Updated

--- HackerRankProblems/collectionsCounter.py
+++ HackerRankProblems/collectionsCounter.py
@@ -4,11 +4,32 @@ if __name__ == "__main__":
     _X = int(input())
     _shoeSizeList = list(map(int,input().split()))
     countShoeSize = Counter(_shoeSizeList)
-    print(countShoeSize)
-    countShoeSize.pop(6)
-    print(countShoeSize)
-    # _N = int(input())
+    # elements = list(countShoeSize.elements())
+    keys = countShoeSize.keys()
+    values = list(countShoeSize.values())
+    print(keys,values)
+    _N = int(input())
+    totalPrice = 0
+    for _ in range(_N):
+        N_customer = list(map(int,input().strip().split(" ")))
+        d_shoeSize,shoePrice = N_customer[0],N_customer[1]
+        for i,item in enumerate(keys):
+            if d_shoeSize == item and values[i]>0:
+                # print("yes")
+                values[i]-=1
+                totalPrice+=shoePrice
+    print(values)
+    print(totalPrice)
+    # print(elements)
+    # totalPrice = 0
+    # 
     # for _ in range(_N):
-    #     N_customer = list(map(int,input().strip().split(" ")))
-    #     d_shoeSize,shoePrice = N_customer[0],N_customer[1]
-        # print(d_shoeSize,shoePrice)
+    #     
+    #     # print(d_shoeSize,shoePrice)
+        
+    #     for item in elements:
+    #         if d_shoeSize == item:
+    #             totalPrice+=shoePrice
+    #             elements.remove(item)
+    # print(totalPrice)
+
