0254392262f2cf145ec9e10e67341094053e3e9c
ShahriarHA <shahriar08neub@gmail.com>
2025-01-25 11:40:06 +0600

    numpy added

---
+++ NumpyTutorial/_1stNumpy.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 25 11:04:03 2025
+
+@author: HP
+"""
+print("hello world!")
+import numpy as np
+
+arr = np.array([1,2,3,4])
+print(arr)
+print((np.__version__))
+
+
+
+
